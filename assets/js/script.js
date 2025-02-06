0f902a5841afc8dc3b78ebba3c043b0e380af3cc
William Italia <williamitalia70@outlook.com>
2025-02-06 14:12:49 -0300

    feat: funcionalidades feitas, e localStorage funcionando

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,12 @@ const btn = document.querySelector('.btn');
 const boxTheme = document.getElementById('box-theme');
 const history = document.getElementById('history');
 const form = document.getElementById('form');
-const itemTitle = document.getElementById('item-title');
-const itemAmount = document.getElementById('item-amount');
+const text = document.getElementById('item-title');
+const amount = document.getElementById('item-amount');
+const balance = document.getElementById('balance');
+const income = document.getElementById('renda');
+const expense = document.getElementById('gasto');
+let transactions = getLocalStorage();
 
 
 function modeTheme() {
@@ -67,6 +71,14 @@ function createMark() {
     return mark;
 }
 
+function generateId() {
+    return Math.floor(Math.random() * 100000000)
+}
+
+function createObj(title, amount) {
+    return {id: crypto.randomUUID(), title: title, amount: amount };
+}
+
 function createItem(title, amount, colorMark) {
 
     const icon = createIcon();
@@ -121,48 +133,158 @@ function formataNum(num) {
 
 }
 
+
+function updateLocalStorage() {
+    localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
+function getLocalStorage() {
+    return JSON.parse(localStorage.getItem('transactions')) || [];
+}
+
 function onAddItemSubmit(e) {
+
     e.preventDefault();
 
+    if(text.value === '' || amount.value === '') {
 
-    const itemT = itemTitle.value;
-    const itemA = itemAmount.value;
+        alert('Por favor preencha todos os campos!');
 
+    } else {
 
-    if (itemT !== '' && itemA !== '') {
-        
-        if(itemA.charAt(0) === '-') {
-            const amount = formataNum(itemA);
-            const item = createItem(itemT, amount, 'bg-red-color');
-            list.prepend(item);
-        }
+        const transaction = createObj(text.value, amount.value);
 
-        if(itemA.charAt(0) > 0) {
-            const amount = formataNum(itemA);
-            const item = createItem(itemT, amount, 'bg-green-color');
-            list.prepend(item);
-        }
+        transactions.push(transaction);
 
-        itemTitle.value = '';
-        itemAmount.value = '';
+        const item = createItem(transaction.title, transaction.amount, transaction.id);
 
-        itemTitle.focus();
+        addItemToDOM(item);
+        updateLocalStorage();
+        updateValues();
+        checkListLength()
+
+        text.value = '';
+        amount.value = '';
+
+        text.focus();
     }
-    
+
+}
+
+function verifiedAmount(amount) {
+
+    const sign = amount < 0 ? `- ${amount.slice(1)}` : `+ ${amount}`;
+    return sign;
+
+}
+
+function createItem(title, amount, id) {
+
+    const icon = createIcon();
+    const li = createLi();
+    const div = createDiv();
+    const span = createSpan();
+    const button = createButton();
+    const mark = createMark();
+
+
+    span.textContent = verifiedAmount(amount);
+    button.appendChild(icon);
+
+    if(amount.charAt(0) == '-') {
+        mark.classList.add('bg-red-color');
+    } else {
+        mark.classList.add('bg-green-color');
+    }
+
+    li.id = id;
+    li.textContent = title;
+
+    div.appendChild(span);
+    div.appendChild(button);
+    li.appendChild(div);
+
+    li.appendChild(mark);
+
+    return li;
+}
+
+
+function addItemToDOM(item) {
+    list.prepend(item);
+}
+
+function removeItemLocalStorage(id) {
+    transactions = transactions.filter(transaction => transaction.id !== id);
+
+    updateLocalStorage();
+    updateValues();
     checkListLength();
+
+}
+
+function onClickItem(e) {
+
+    if(e.target && e.target.parentElement.classList.contains('remove-item')) {
+        const item = e.target.closest('li');
+
+        const id = item.getAttribute('id');
+        
+        removeItemLocalStorage(id);
+        item.remove();
+
+        updateLocalStorage();
+        checkListLength();
+    }
+}
+
+function displayItems() {
+    const itemsFromStorage = getLocalStorage();
+
+    
+    itemsFromStorage.forEach((item) => {
+        const I = createItem(item.title, item.amount, item.id);
+        addItemToDOM(I);
+    });
+
+    checkListLength();
+}
+
+function updateValues() {
+
+    let transactions = getLocalStorage();
+
+    const amounts = transactions.map(transaction => Number(transaction.amount));
+
+    if(amounts.length != '') {
+        
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+    const renda = amounts.filter((item) => item > 0).reduce((acc, item) => (acc += item), 0).toFixed(2);
+    const gasto = amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0).toFixed(2);
+    console.log(total, renda);
+    
+    balance.innerHTML = total;
+    income.innerHTML = renda;
+    expense.innerHTML = gasto;
+        
+    }
+
 }
 
 function init() {
-
     boxTheme.addEventListener('click', modeTheme);
     form.addEventListener('submit', onAddItemSubmit);
+    list.addEventListener('click', onClickItem);
+    document.addEventListener('DOMContentLoaded', displayItems);
+    
     checkListLength();
-}
+    updateValues();
+    
+} 
 
 
 init();
-
-
 
 
 
